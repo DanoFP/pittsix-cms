0e2ca95b98c52bd9ce012f0e8488ee51a74a267f
Danilo Piñeyro <dpineyro13@gmail.com>
2025-04-28 23:01:56 -0300

    Add background to jumbotron 4k

--- frontend/src/components/Jumbotron3D.tsx
+++ frontend/src/components/Jumbotron3D.tsx
@@ -10,7 +10,7 @@ export default function Jumbotron3D() {
     <section className="relative h-screen flex items-center justify-center overflow-hidden bg-black">
       {/* Background - Cielo estrellado */}
       <motion.img
-        src="/assets/jumbotron/background1.png"
+        src="/assets/jumbotron/Pittsix 4k.png"
         alt="Background"
         className="absolute w-full h-full object-cover"
         style={{ zIndex: 1 }}
@@ -20,18 +20,18 @@ export default function Jumbotron3D() {
       />
 
       {/* Midground - Curvas de energía */}
-      <motion.img
-        src="/assets/jumbotron/midground2.png"
+      {/* <motion.img
+        src="/assets/jumbotron/Pittsix 4k.png"
         alt="Midground"
         className="absolute w-4/5 md:w-3/4 object-contain"
         style={{ zIndex: 2 }}
         initial={{ y: 0 }}
         animate={{ y: [0, 10, 0] }}
         transition={{ duration: 12, repeat: Infinity, repeatType: "mirror" }}
-      />
+      /> */}
 
       {/* Foreground - Logo PittSix */}
-      <motion.img
+      {/* <motion.img
         src="/assets/jumbotron/foreground1.png"
         alt="Foreground"
         className="absolute w-1/2 md:w-1/3 object-contain"
@@ -39,7 +39,7 @@ export default function Jumbotron3D() {
         initial={{ scale: 1 }}
         animate={{ scale: [1, 1.02, 1] }}
         transition={{ duration: 8, repeat: Infinity, repeatType: "mirror" }}
-      />
+      /> */}
 
     </section>
   );
